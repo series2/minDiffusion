018f4f63e8d661999bb2bca60247d91f7695d661
series2 <series2.others@gmail.com>
2023-07-27 10:21:10 +0900

    add example

---
+++ execute_example.sh
@@ -0,0 +1,5 @@
+python diffusion.py \
+    --dataset_name Psude1dimDataset \
+    --data_dir data/psudedata/100000size-1dim-1gmm-origin05 \
+    --eps_model_name FFNModel \
+    --result_dir ./runs/debug
